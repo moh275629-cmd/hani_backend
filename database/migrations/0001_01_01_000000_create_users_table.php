4067923ee75a6f4f109d22e333d649322c0a5bb4
mohamed12501 <mohamedoudha82@gmail.com>
2025-08-25 21:49:38 +0100

    Deploy to BLOB

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->string('role')->default('client');
-            $table->longBlob('profile_image')->nullable();
+            $table->longText('profile_image')->nullable();
             $table->longText('profile_image_blob')->nullable();
             $table->string('state')->nullable();
             $table->string('state_code')->nullable();
