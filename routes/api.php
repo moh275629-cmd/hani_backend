c987866b0abca66f53c58a61cffc2bc1f9f56d77
mohamed12501 <mohamedoudha82@gmail.com>
2025-09-11 17:00:13 +0100

    admin fixes

--- routes/api.php
+++ routes/api.php
@@ -147,14 +147,6 @@ Route::get('/images/temp/{tempId}', [ImageController::class, 'serveTempImage']);
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
-    // Global Admin - Admins management
-    Route::middleware('can:manage-global')->group(function () {
-        Route::get('/global-admin/admins', [AdminController::class, 'index']);
-        Route::post('/global-admin/admins', [AdminController::class, 'store']);
-        Route::get('/global-admin/admins/{id}', [AdminController::class, 'show']);
-        Route::put('/global-admin/admins/{id}', [AdminController::class, 'update']);
-        Route::delete('/global-admin/admins/{id}', [AdminController::class, 'destroy']);
-    });
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::put('/profile', [AuthController::class, 'updateProfile']);
@@ -295,6 +287,16 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // Global Admin routes
     Route::prefix('global-admin')->middleware('global.admin')->group(function () {
+        // Admin office info CRUD (new) - separate from user admin management
+        Route::prefix('admin-offices')->group(function () {
+            Route::get('/', [AdminController::class, 'index']);
+            Route::post('/', [AdminController::class, 'store']);
+            Route::get('/{id}', [AdminController::class, 'show']);
+            Route::put('/{id}', [AdminController::class, 'update']);
+            Route::put('/user/{userId}', [AdminController::class, 'updateByUser']);
+            Route::delete('/{id}', [AdminController::class, 'destroy']);
+        });
+        // Global admin user management routes (existing)
         Route::get('/admins', [GlobalAdminController::class, 'admins']);
         Route::post('/admins', [GlobalAdminController::class, 'createAdmin']);
         Route::put('/admins/{admin}', [GlobalAdminController::class, 'updateAdmin']);
